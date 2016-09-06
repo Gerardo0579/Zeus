1809979a002be22a3f195391e68c60928c5c7790
Fercho <gomez.alejandre@gmail.com>
2016-09-05 23:55:32 -0500

    Interfaz
    Primeras versiones de la intefaz

---
+++ Interfaz.py
@@ -0,0 +1,12 @@
+from tkinter import *
+#Nueva ventana
+ventana = Tk()
+
+#Dimension de ventana
+ventana.geometry("800x600")
+
+#Titulo de la ventana
+ventana.title("Rubricas")
+
+#Para los usuarios de windows linux no lo necesita
+ventana.mainloop()
