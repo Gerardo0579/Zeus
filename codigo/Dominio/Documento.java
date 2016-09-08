e93067d36ba6dbed8939e341063a0f86ba2ec2f5
Gerardo0579 <gerardo0579@hotmail.com>
2016-09-07 23:12:03 -0500

    Código generado
    El diseño cambió el nombre de las carpetas y se generó el código de
    Enterprise

---
+++ codigo/Dominio/Documento.java
@@ -0,0 +1,19 @@
+package Dominio;
+
+
+/**
+ * @author DARKENSES
+ * @version 1.0
+ * @created 07-sep.-2016 11:09:43 p. m.
+ */
+public class Documento {
+
+	public Documento(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+}
